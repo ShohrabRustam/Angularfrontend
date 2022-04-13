73e3529c6b6a96114c7dd12f48df56dfc6d33b6f
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-13 14:03:55 +0530

    make child component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
 import { PublicComponent } from './public/public.component';
 import { SecureComponent } from './secure/secure.component';
 
 const routes: Routes = [
   {
     path:"public",
-    component:PublicComponent
+    component:PublicComponent,
+    children:[
+      {
+        path:'home',
+        component:HomeComponent
+      }
+
+    ]
   },
   {
     path:"secure",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { PublicComponent } from './public/public.component';
 import { SecureComponent } from './secure/secure.component';
+import { HomeComponent } from './public/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     PublicComponent,
-    SecureComponent
+    SecureComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
