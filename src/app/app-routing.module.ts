aff5d9a1f9991ab08254a20002a48d632f3b6997
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-13 14:01:45 +0530

    secure component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"public",
+    component:PublicComponent
+  },
+  {
+    path:"secure",
+    component:SecureComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
